4c7eb41a050ead357702cc2b3f77e1003ce1a2b6
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2026-04-21 15:51:46 +0600

    test: add pest architecture rules (dto, contracts, exceptions)

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -3,3 +3,22 @@
 arch('no debug calls')
     ->expect(['dd', 'dump', 'ray', 'var_dump'])
     ->not->toBeUsed();
+
+arch('DTOs are readonly value classes')
+    ->expect('DevWizard\Payify\Dto')
+    ->toBeReadonly()
+    ->toBeFinal();
+
+arch('contracts are interfaces')
+    ->expect('DevWizard\Payify\Contracts')
+    ->toBeInterfaces();
+
+arch('exceptions extend PayifyException')
+    ->expect('DevWizard\Payify\Exceptions')
+    ->classes()
+    ->toExtend(\DevWizard\Payify\Exceptions\PayifyException::class)
+    ->ignoring(\DevWizard\Payify\Exceptions\PayifyException::class);
+
+arch('no cross-layer coupling into Http controllers from DTOs')
+    ->expect('DevWizard\Payify\Dto')
+    ->not->toUse('DevWizard\Payify\Http\Controllers');
